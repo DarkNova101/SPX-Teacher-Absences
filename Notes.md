e06bc8c3b3b6e0b612e08efed2bed022bbe46375
Dark <nehall.neh@gmail.com>
2019-03-26 22:31:42 -0500

    Initial Groundwork Testing

---
+++ Notes.md
@@ -0,0 +1,5 @@
+PHP & Curl = http://usefulangle.com/post/9/google-login-api-with-php-curl
+
+
+
+JS API = http://usefulangle.com/post/55/google-login-javascript-api
---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '392577016618-974b5j6u9kib7pllj11k33q3krc6nv1m.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'JYNYL_r4twSi0LRsWVopBhAQ');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/gauth.php');
+
