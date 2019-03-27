3988eb2a01f5fda579333c8c79f5c786ce776647
Dark <nehall.neh@gmail.com>
2019-03-26 22:45:08 -0500

    Updated gauth for sessions.

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
+session_start();
 
 require_once('settings.php');
+
 
 ?>
 <html>
@@ -17,8 +19,17 @@ require_once('settings.php');
 </head>
 
 <body>
+<?php
+if (!isset($_SESSION['user_name']) || !isset($_SESSION['logged_in']) ) {
+    print_r($_SESSION);
+    ?>
+    <a id="login-button" href="<?= 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email') . '&redirect_uri=' . urlencode(CLIENT_REDIRECT_URL) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=online' ?>">Login with Google</a>
+<?php
+} else {
+    print_r($_SESSION);
+    echo "Logged In!";
+}
 
-<a id="login-button" href="<?= 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email') . '&redirect_uri=' . urlencode(CLIENT_REDIRECT_URL) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=online' ?>">Login with Google</a>
-
+?>
 </body>
 </html>
